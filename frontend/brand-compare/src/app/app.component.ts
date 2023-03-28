5a2c649b61e90d0f2f9fcb8a2a909272c135976f
GabrielaD <david.gabriela.99@gmail.com>
2023-03-28 13:52:11 +0300

    Angular material setup

--- frontend/brand-compare/src/app/app.component.ts
+++ frontend/brand-compare/src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+export interface Brand {
+  name: string;
+  totalProfiles: number;
+  totalFans: number;
+  totalEngagement: number;
+}
+
+const ELEMENT_DATA: Brand[] = [
+  { name: 'Nike', totalProfiles: 2, totalFans: 1000, totalEngagement: 4000 },
+  { name: 'Gucci', totalProfiles: 4, totalFans: 1000, totalEngagement: 4000 },
+  {
+    name: 'Adidas',
+    totalProfiles: 3,
+    totalFans: 1000,
+    totalEngagement: 4000,
+  },
+  {
+    name: 'Balenciaga',
+    totalProfiles: 3,
+    totalFans: 1000,
+    totalEngagement: 4000,
+  },
+  { name: 'Guess', totalProfiles: 1, totalFans: 1000, totalEngagement: 4000 },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'brand-compare';
+  displayedColumns: string[] = [
+    'name',
+    'totalProfiles',
+    'totalFans',
+    'totalEngagement',
+  ];
+  dataSource = ELEMENT_DATA;
+
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 }
--- frontend/brand-compare/src/app/app.module.ts
+++ frontend/brand-compare/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
