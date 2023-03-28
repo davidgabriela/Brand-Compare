747dab58ae47be42992b693fb4d67f47fbc95e98
GabrielaD <david.gabriela.99@gmail.com>
2023-03-28 17:53:16 +0300

    Process data from API

---
+++ README.md
@@ -0,0 +1,63 @@
+Came across a bug: Using get_profile_data for a twitter account has unexpected behaviour:
+
+1. The fields: followers, following and following_change disappear on the entry corresponding to end date. 
+2. Besides the entries for the days in the given range, more entries for days that are not in the range appear in the following format '[other_date]': { date: '[other_date]', following_change: 0 }
+
+Similar behaviour was found for instagram_profile for some start-end ranges. For the following request:
+{
+    "id" : 1,
+    "method" : "socialinsider_api.get_profile_data",
+    "params":{
+        "id":"nike",
+        "profile_type": "instagram_profile",
+        "date": {
+            "start": 1608209422374,
+            "end": 1608300000000,
+            "timezone": "Europe/London"
+        }
+    }
+}
+
+The response is: 
+
+{
+    "id": 1,
+    "error": null,
+    "resp": {
+        "nike": {
+            "17-12-2020": {
+                "date": "17-12-2020",
+                "likes": 0,
+                "comments": 0,
+                "followers": 124495434,
+                "reach": 0,
+                "impressions": 0,
+                "engagement": 0,
+                "avg_eng_per_post": 0,
+                "eng_rate_per_profile": 0,
+                "eng_rate_per_post": 0,
+                "video_views": 0,
+                "posts": 0
+            },
+            "18-12-2020": {
+                "date": "18-12-2020",
+                "likes": 0,
+                "comments": 0,
+                "followers": 124707275,
+                "reach": 0,
+                "impressions": 0,
+                "engagement": 0,
+                "followers_net_diff_evolution": 211841,
+                "avg_eng_per_post": 0,
+                "eng_rate_per_profile": 0,
+                "eng_rate_per_post": 0,
+                "video_views": 0,
+                "posts": 0
+            },
+            "17-11-2020": {
+                "date": "17-11-2020",
+                "followers": 124495434
+            }
+        }
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,63 @@ const PORT = 3000
 const API_KEY = 'API_KEY_TEST'
 const URL = 'https://app.socialinsider.io/api'
 
+async function getData() {
+  let brandData = await getBrands()
+
+  brandData.map((brand) => {
+    const brandName = brand.brandname
+    const brandProfiles = brand.profiles
+    let totalFans = 0
+    let totalEngagement = 0
+
+    brandProfiles.map(async (profile) => {
+      const profileId = profile.id
+      const profileType = profile.profile_type
+      const date = {
+        start: 1608209422374,
+        end: 1608299422374,
+        timezone: 'Europe/London',
+      }
+
+      const profileData = await getProfileData(profileId, profileType, date)
+
+      totalFans = Object.keys(profileData).reduce((total, key) => {
+        if ('followers' in profileData[key])
+          return total + profileData[key].followers
+        else return total
+      }, totalFans)
+
+      totalEngagement = Object.keys(profileData).reduce((total, key) => {
+        if ('engagement' in profileData[key])
+          return total + profileData[key].engagement
+        else return total
+      }, totalEngagement)
+
+      console.log(
+        brandName,
+        profileId,
+        profileType,
+        date,
+        totalFans,
+        totalEngagement,
+        profileData,
+        '\n\n',
+      )
+    })
+
+    const data = {
+      brandName,
+      brandProfiles: brandProfiles.length,
+      totalFans,
+      totalEngagement,
+    }
+
+    console.log(data)
+    return data
+  })
+  return brandData
+}
+
 async function getBrands() {
   const config = {
     headers: {
@@ -24,11 +81,10 @@ async function getBrands() {
     },
   }
   const response = await axios.post(URL, data, config)
-  console.log(response.data)
   return response.data.result
 }
 
-async function getProfileData() {
+async function getProfileData(profileId, type, date) {
   const config = {
     headers: {
       'Content-Type': 'application/json',
@@ -40,22 +96,18 @@ async function getProfileData() {
     id: 0,
     method: 'socialinsider_api.get_profile_data',
     params: {
-      id: '44596321012',
-      profile_type: 'facebook_page',
-      date: {
-        start: 1608209422374,
-        end: 1639745412436,
-        timezone: 'Europe/London',
-      },
+      id: profileId,
+      profile_type: type,
+      date,
     },
   }
   const response = await axios.post(URL, data, config)
-  console.log(response.data)
-  return response.data.result
+  return response.data.resp[profileId]
 }
 
-getBrands()
-getProfileData()
+// getBrands()
+// getProfileData(44596321012, 'facebook_page', 1608209422374, 1639745412436)
+getData()
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}/`)
